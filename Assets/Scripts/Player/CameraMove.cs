8ecd787c96b9478da159002f61c7bda72e8378e1
lookie4k <lookie4k@naver.com>
2017-01-19 16:15:49 +0900

    Joystick and Camera Update

---
+++ Assets/Scripts/Player/CameraMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public SpriteRenderer background;
+    public Transform player;
+
+    private Vector3 limitPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        InitLimitPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        Move();
+    }
+
+    private void InitLimitPosition()
+    {
+        Vector3 mapSize = background.bounds.extents;
+
+        Camera mainCamera = Camera.main;
+        float cameraHeight = mainCamera.orthographicSize;
+        float cameraWidth = mainCamera.aspect * cameraHeight;
+
+        limitPosition = new Vector3(mapSize.x - cameraWidth, mapSize.y - cameraHeight, transform.position.z);
+    }
+
+    private void Move()
+    {
+        Vector3 playerPosition = player.position;
+        playerPosition.z = transform.position.z;
+
+        transform.position = ClampVector(playerPosition, -1 * limitPosition, limitPosition);
+    }
+
+    private Vector3 ClampVector(Vector3 value, Vector3 min, Vector3 max)
+    {
+        return Vector3.Min(Vector3.Max(value, min), max);
+    }
+}
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerMove : MonoBehaviour {
 
     private Rigidbody2D playerRigidbody;
+    private Vector3 defaultLoc;
 
     public float speed;
+    public RectTransform aimJoystick;
 
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        defaultLoc = (aimJoystick.position - new Vector3(0, 0, 0));
     }
 
     void Update () {
@@ -20,18 +24,20 @@ public class PlayerMove : MonoBehaviour {
 
     private void Move()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
+        float x = CrossPlatformInputManager.GetAxisRaw("MoveHorizontal");
+        float y = CrossPlatformInputManager.GetAxisRaw("MoveVertical");
 
         Vector3 vector = new Vector3(x, y);
-
-        vector.Normalize();
 
         playerRigidbody.velocity = vector * speed;
     }
 
     private void Rotate()
     {
-        // TODO   after joystick complete
+        Vector3 distance = aimJoystick.position - defaultLoc;
+        float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
+        if (angle == 0)
+            return;
+        transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
     }
 }
